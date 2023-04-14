92fe8dfea14e6827a5fb354bcb18b867b22ab1bd
Raissa <raissamasi2@gmail.com>
2023-04-14 15:16:48 -0300

    UserService e registro de componente

---
+++ src/main/java/com/mavenproject/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.mavenproject.course.services;
+
+import com.mavenproject.course.entities.User;
+import com.mavenproject.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service // registra a classe como componente para que possa ser injetada com autowired
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
