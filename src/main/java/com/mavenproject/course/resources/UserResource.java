c2debfd48b7a4079a3d1e39c21f370dcdd9fe963
Raissa <raissamasi2@gmail.com>
2023-04-14 14:29:43 -0300

    Repositório JPA, injeção de dependência e populando banco

--- src/main/java/com/mavenproject/course/resources/UserResource.java
+++ src/main/java/com/mavenproject/course/resources/UserResource.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(value = "/users")
+@RestController // diz que é resurso web implementado por controlador rest
+@RequestMapping(value = "/users") // nome do recurso
 public class UserResource {
 
-    @GetMapping
+    @GetMapping // diz que responde a requisição get do http
     public ResponseEntity<User> findAll(){
         User u = new User(1L, "Maria", "maria@gmail.com", "99129931", "12345");
         return ResponseEntity.ok().body(u);
